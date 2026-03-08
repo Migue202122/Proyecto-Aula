16dc5e243c386c5306b173669ec795455879eb7b
AndresPalacioITM <andrespalacio307239@correo.itm.edu.co>
2026-03-07 23:38:01 -0500

    Parte 14: Agregar AutenticacionControlller y ConfiguracionJwt - login con JWT

--- obj/Debug/net9.0/ApiGenericaChsarp.AssemblyInfo.cs
+++ obj/Debug/net9.0/ApiGenericaChsarp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGenericaChsarp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5d34ed1e60e88f281d325df8fac82501a08a544")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4751eed7ad1d9cdfd7d4eaa1cfcdc8323f8376ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGenericaChsarp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGenericaChsarp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
