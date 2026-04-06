724e1aeb52388e5a200763493ce7a804db868bed
AndresPalacioITM <andrespalacio307239@correo.itm.edu.co>
2026-04-05 20:40:41 -0500

    .

--- obj/Debug/net9.0/ApiGenericaCsharp.AssemblyInfo.cs
+++ obj/Debug/net9.0/ApiGenericaCsharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGenericaCsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43ccae90050d69e9048c715d922181b76c5b165f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a75cf1d3e7c2716a0952f65f58a9cb9f49df99a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGenericaCsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGenericaCsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
